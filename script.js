359b772a0532d272d573fb5c62cfaa874eaea9d1
github username <avelonpang@gmail.com>
2021-04-28 16:18:07 -0500

    added nav bar

--- script.js
+++ script.js
@@ -1,74 +1,82 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
 
-let apiQuotes = [];
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
-//Show Loading
-function loading(){
-    loader.hidden = false; 
-    quoteContainer.hidden = true; 
+let quoteBox = [];
+
+ // Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
 }
 
-//Hide Loading
-function complete(){
-    loader.hidden = true; 
-    quoteContainer.hidden = false; 
+// Hide loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
 }
 
-
+//Show New Quote
 function newQuote() {
     loading();
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-
+    // Pick a random quote from data array
+    const quote = quoteBox[Math.floor(Math.random()*quoteBox.length)];
+    // Check if Author is none and replace it with "Unknown"
     if (!quote.author) {
-        authorText.textContent = 'Unknown';
+        authorText.textContent = "Unknown";
     } else {
         authorText.textContent = quote.author;
     }
-    if (quote.text.length > 120) {
-        quoteText.classList.add('long-quote');
+    // Styling for long quotes
+    if (quote.text.length > 50) {
+        quoteText.classList.add("long-quote");
     } else {
-        quoteText.classList.remove('long-quote');
+        quoteText.classList.remove("long-quote");
     }
-    quoteText.textContent = quote.text;
-    complete(); //SetQuote, Hide Loader 
+    // Set Quote, Hide Loader
+    quoteText.textContent = quote.text;  
+    complete();
 }
 
-
-async function getQuotes() {
+// Get Quote From API 
+async function getQuote() {
     loading();
-    const apiURL = 'https://type.fit/api/quotes';
+    const apiUrl = "https://type.fit/api/quotes";
     try {
-        const response = await fetch(apiURL);
-        apiQuotes = await response.json();
+        const response = await fetch(apiUrl);
+        quoteBox = await response.json();
         newQuote();
     } catch (error) {
-
-    }
+        quote();
+        console.log("whoops, no quote", error);
+    }   
 }
 
+// Tweet Quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`; 
-    window.open(twitterUrl, '_blank'); //open twitter in new tab
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
 }
 
-
+// Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
-twitterBtn.addEventListener('click', tweetQuote)
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuote();
 
 
-getQuotes();
 
 
-/* If using local quotes via quotes.js:
 
-function newQuote(){
-const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-console.log(quote)
-}
 
-*/
+
+
+
+
+
+
